721120b3e67891ac4bed62688a9b8dd5ba80ed06
tohumcuugur <ugurtohumcuu@gmail.com>
2023-02-09 23:22:07 +0300

    shop page update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CardProvider } from './contexts/card.context';
 
 
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CardProvider>
             <App />
           </CardProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
 );
